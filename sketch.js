4c5c452d970748f1f9b78195eee92e45d9194fec
ArchitVerma1812 <75210529+ArchitVerma1812@users.noreply.github.com>
2020-12-13 15:58:56 +0530

    Update sketch.js
    gsco

--- sketch.js
+++ sketch.js
@@ -3,21 +3,23 @@ var speed, weight;
 var deformation;
 
 function setup() {
-  createCanvas(800,400);
+  createCanvas(1600,400);
   car=createSprite(50, 200, 50, 50);
-  wall=createSprite(1500,200,60,height/2);
-  speed=Math.round(random(55,90));
+  wall=createSprite(1500,200,20,height/2);
+  speed=Math.round(random(55,100));
   weight=Math.round(random(400,1500));
   car.velocityX=speed;
 }
 
 function draw() {
-  background(255,255,255);  
-  if(car.x-wall.x=car.width/2+wall.width/2)
+  background("black");  
+  if(wall.x-car.x < (car.width+wall.width)/2)
   {
-    deformation=(0.5 * weight * speed * speed)/22500;
-  }
-  if(deformation<100)
+    car.velocityX=0;
+    car.x=wall.x-30;
+    deformation=(0.5 * weight * speed * speed)/22509;
+    console.log(deformation);
+    if(deformation<100)
   {
     car.shapeColor="green";
   }
@@ -28,5 +30,7 @@ function draw() {
   else{
     car.shapeColor="red";
   }
+  }
+  
   drawSprites();
 }
